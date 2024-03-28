ace583273cf2053ad8e4d7bf5ecd7026e8ed4c28
Anton <antoncodes@outlook.com>
2024-03-28 20:09:40 +0200

    header content mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
+import logo from './logo.svg';
+
 export default function RouteHome() {
   return (
     <div className="flex flex-col min-h-[100vh]">
-      <header style={{ height: 88 }}>header</header>
+      <header
+        className="
+        container
+        flex
+        items-center
+        justify-between
+        h-[5.5rem]
+      "
+      >
+        <button>
+          <Image alt="Home" src={logo} />
+        </button>
+        <button className="inline-flex px-1">
+          <span className="material-symbols-rounded text-4xl">menu</span>
+        </button>
+      </header>
       <main className="flex-1 bg-gray-200">main</main>
       <footer className="bg-black text-white">
         <span>footer</span>
