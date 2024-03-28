d43a720d31c856406d3f73c816475a263537f728
Anton <antoncodes@outlook.com>
2024-03-28 20:34:11 +0200

    footer logo init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,15 @@ export default function RouteHome() {
       "
       >
         <button>
-          <Image alt="Home" src={logo} />
+          <Image alt="Go to Homepage" src={logo} />
         </button>
         <button className="inline-flex px-1">
           <span className="material-symbols-rounded text-4xl">menu</span>
         </button>
       </header>
       <main className="flex-1 bg-gray-200">main</main>
-      <footer className="bg-black text-white">
-        <span>footer</span>
+      <footer className="container text-center bg-black text-white py-12">
+        <Image alt="Cyber Logo" src={logo} className="mx-auto invert" />
       </footer>
     </div>
   );
